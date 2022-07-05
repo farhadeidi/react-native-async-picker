bb3240b87498fbe13d178af695a2132bdb20ab99
Farhad <farcom7@gmail.com>
2022-07-05 12:52:03 +0300

    Fix typescript issues

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import AsyncPickerComponent from './AsyncPicker';
 import { AsyncPickerProvider } from './utils/AsyncPickerContext';
-import PickerItem, { PickerItemProps } from './components/PickerItem';
+import PickerItem from './components/PickerItem';
+import type { PickerItemProps } from './components/PickerItem';
 import type { AsyncPickerProps, AsyncPickerRef } from './types';
 import * as helpers from './utils/helpers';
 import twColors from './utils/twColors';
-import {
-  defaultLightColors,
-  defaultDarkColors,
-  ColorProps,
-} from './utils/colors';
+import { defaultLightColors, defaultDarkColors } from './utils/colors';
+import type { ColorProps } from './utils/colors';
 
 declare module 'react' {
   function forwardRef<T, P = {}>(
